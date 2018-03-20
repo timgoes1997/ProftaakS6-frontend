0c18427028e24682225969dfb6e650f5db4cd48a
biepbot <biepbot@gmail.com>
2018-03-20 19:07:44 +0100

    Vehicle upload changes

--- WEB-INF/js/map.js
+++ WEB-INF/js/map.js
@@ -231,16 +231,16 @@ function definePopup() {
     m.addInput('text', 'Model', 'model', null, function(e) {
         return e.value != 0;
     });
-    m.addInput('text', 'Nummerplaat', 'license', null, function(e) {
+    m.addInput('text', 'Nummerplaat', 'licenseplate', null, function(e) {
         return e.value != 0;
     });
-    m.addInput('date', 'Bouwdatum', 'builddate', null, function(e) {
+    m.addInput('date', 'Bouwdatum', 'buildDate', null, function(e) {
         return e.value != 0;
     });
     m.addSpace(30);
     m.addDivider();
     m.addTitle(3, 'Persoonlijke informatie');
-    m.addInput('file', 'Eigendomsbewijs', 'poo', null, function(e) {
+    m.addInput('file', 'Eigendomsbewijs', 'file', null, function(e) {
         return true;
     });
     m.addDivider();
@@ -250,21 +250,14 @@ function definePopup() {
         'onsubmit': function (e) {
             if (m.verified()) {
                 // send
-                var xhr = new XMLHttpRequest() ;
-                xhr.open('POST', API_PATH + 'vehicle/new'); 
-                xhr.onload = function (e) { 
-                    notify('Car was succesfully added', notif.defaultTime); 
-                }; 
-                xhr.onerror = function(e) {
-                    if (xhr.status === 0) {
-                        notify('Could not add car: connection refused', 'error', notif.longTime);
-                    } else 
-                        notify('Could not add car: ERROR ' + xhr.status, 'error', notif.longTime);
-                };
-                var formData = new FormData(m.getForm()); 
-                xhr.send(formData);
-                m.close();
                 notify('Uploading new car...', 'info', notif.defaultTime);
+                call('POST', API_PATH + 'vehicle/new', new FormData(m.getForm()), function(e,succ) {
+                    if (succ) {
+                        notify('Car was succesfully added', 'success', notif.defaultTime); 
+                    } else {
+                        notify('Could not add car', 'error', notif.longTime);
+                    }
+                })
             } else {
                 notify('Please fill in all fields', 'warning', notif.longTime);
             }
