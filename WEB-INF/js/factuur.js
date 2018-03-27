b918b644d770f2bd42e71c55eab8cbfbe638c2d5
biepbot <biepbot@gmail.com>
2018-03-27 15:24:47 +0200

    Factuur details worden ingeladen (simpel)

--- WEB-INF/js/factuur.js
+++ WEB-INF/js/factuur.js
@@ -3,4 +3,21 @@ addEvent(window, 'load', init);
 
 function init() {
     var params = getQueryParams();
+    call('GET', API_PATH + 'bills/' + params.id, null, function (e, succ) {
+        if (succ) {
+            e = JSON.parse(e);
+            setValue('factuurnummer', e.billnr);
+            setValue('nummerplaat', e.license);
+            setValue('totaalbedrag', e.price + ' ' + e.currency);
+            setValue('status', e.status);
+            setValue('maand', e.month);
+        } else {
+            // todo 
+            //take to other page?
+        }
+    });
+}
+
+function setValue(name, value) {
+    $(name).children[1].innerHTML = value;
 }
