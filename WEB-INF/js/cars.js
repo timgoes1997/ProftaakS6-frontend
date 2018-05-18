e97fe0bef044ba91bdcd03881b9c09b01971fe42
biepbot <biepbot@gmail.com>
2018-05-18 14:09:47 +0200

    car transfer uses confirmation modals

---
+++ WEB-INF/js/cars.js
@@ -0,0 +1,9 @@
+function removeCar(license) {
+    addClass(confirm('Verwijder', function (e) {
+        // logic for removing cars here
+    }, 'Weet u zeker dat u auto met nummerplaat <b>' + license + '</b> wilt verwijderen?'), 'mini-modal');
+}
+
+function transferCar(license) {
+
+}
